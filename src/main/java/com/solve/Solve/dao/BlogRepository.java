4c433c4a0efcda40cb3100a02079a70abd73d240
shldhll <iam.shldhll@gmail.com>
2021-05-19 21:09:01 +0530

    changed java version

--- src/main/java/com/solve/Solve/dao/BlogRepository.java
+++ src/main/java/com/solve/Solve/dao/BlogRepository.java
@@ -2,7 +2,9 @@ package com.solve.Solve.dao;
 
 import com.solve.Solve.model.Blog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BlogRepository extends JpaRepository<Blog, Integer> {
 
 }
