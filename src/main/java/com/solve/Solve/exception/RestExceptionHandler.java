bf3486e81ad7f7d40f6470d2bd19f899740dff01
shldhll <iam.shldhll@gmail.com>
2021-05-20 03:05:37 +0530

    custom error handler for invalid data

--- src/main/java/com/solve/Solve/exception/RestExceptionHandler.java
+++ src/main/java/com/solve/Solve/exception/RestExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.solve.Solve.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -18,5 +21,12 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     public final ResponseEntity<ErrorMessage> handleIncompleteJsonException(IncompleteJsonException exception) {
         ErrorMessage errorMessage = new ErrorMessage(HttpStatus.BAD_REQUEST, exception.getMessage());
         return new ResponseEntity<>(errorMessage, errorMessage.getStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String errorMessage = ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+        ErrorMessage exceptionMessage = new ErrorMessage(HttpStatus.BAD_REQUEST, errorMessage);
+        return new ResponseEntity<>(exceptionMessage, exceptionMessage.getStatus());
     }
 }
